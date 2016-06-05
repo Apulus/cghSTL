e9878380c14d5c5452335386dd934fa37464d2b8
CHEN Gonghao <chengonghao@yeah.net>
2016-06-05 21:59:39 +0800

    增加 cgh_memmove 和 cgh_memcpy 函数

--- cghUtil/cghUtil.h
+++ cghUtil/cghUtil.h
@@ -7,10 +7,11 @@
 *  内容:cghSTL的工具类
 ******************************************************************/
 
-#ifndef _CGH_PAIR
-#define _CGH_PAIR
+#ifndef _CGH_UTIL_
+#define _CGH_UTIL_
 
 namespace CGH{
+	/* 自定义的pair */
 	template<class T1,class T2>
 	struct cghPair {
 		typedef T1	first_type;
@@ -23,11 +24,52 @@ namespace CGH{
 		cghPair(const T1& a, const T2&b) :first(a), second(b){}
 	};
 
+	/* 交换函数 */
 	template <class T> 
 	void swap(T& a, T& b)
 	{
 		T c(a); a = b; b = c;
 	}
-}
 
+	/* 复制函数 */
+	void * __cdecl cgh_memmove ( void * dst, const void * src, size_t count)
+	{
+		void * ret = dst;
+		if (dst <= src || (char *)dst >= ((char *)src + count))
+		{
+			// 若dst和src区域没有重叠，则从起始处开始逐一拷贝
+			while (count--)
+			{
+				*(char *)dst = *(char *)src;
+				dst = (char *)dst + 1;
+				src = (char *)src + 1;
+			}
+		}
+		else
+		{ // 若dst和src 区域交叉，则从尾部开始向起始位置拷贝，这样可以避免数据冲突
+			dst = (char *)dst + count - 1;
+			src = (char *)src + count - 1;
+			while (count--)
+			{
+				*(char *)dst = *(char *)src;
+				dst = (char *)dst - 1;
+				src = (char *)src - 1;
+			}
+		}
+		return(ret);
+	}
+
+	/* 复制函数 */
+	void * __cdecl cgh_memcpy(void * dst, const void * src, size_t count)
+	{
+		void * ret = dst;
+		while (count--)
+		{ // 注意， cgh_memcpy函数没有处理dst和src区域是否重叠的问题
+			*(char *)dst = *(char *)src;
+			dst = (char *)dst + 1;
+			src = (char *)src + 1;
+		}
+		return(ret);
+	}
+}
 #endif
