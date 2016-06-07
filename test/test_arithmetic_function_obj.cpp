e9c0e4d858b4daf0a283cca02f8aa2189f1a8374
CHEN Gonghao <chengonghao@yeah.net>
2016-06-07 10:24:00 +0800

    测试算术类函数对象

---
+++ test/test_arithmetic_function_obj.cpp
@@ -0,0 +1,46 @@
+/*******************************************************************
+*  Copyright(c) 2016 Chen Gonghao
+*  All rights reserved.
+*
+*  chengonghao@yeah.net
+*
+*  文件内容：测试算术类（Arthmetic）函数对象（仿函数）
+******************************************************************/
+
+#include "stdafx.h"
+//#include <iostream>
+//#include "cgh_function_obj.h"
+//
+//
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	using namespace::CGH;
+//
+//	plus<int> plusObj;
+//	minus<int> minusObj;
+//	multiplies<int> multipliesObj;
+//	divides<int> dividesObj;
+//	modulus<int> modulusObj;
+//	negate<int> nagateObj;
+//
+//	std::cout << "************************ 测试函数对象1 ***************************" << std::endl << std::endl;
+//
+//	std::cout << "plusObj(3, 5) = " << plusObj(3, 5) << std::endl << std::endl;
+//	std::cout << "minusObj(3, 5) = " << minusObj(3, 5) << std::endl << std::endl;
+//	std::cout << "multipliesObj(3, 5) = " << multipliesObj(3, 5) << std::endl << std::endl;
+//	std::cout << "dividesObj(3, 5) = " << dividesObj(3, 5) << std::endl << std::endl;
+//	std::cout << "modulusObj(3, 5) = " << modulusObj(3, 5) << std::endl << std::endl;
+//	std::cout << "nagateObj(3) = " << nagateObj(3) << std::endl << std::endl;
+//
+//	std::cout << "************************* 测试函数对象2 **************************" << std::endl << std::endl;
+//
+//	std::cout << "plus<int>()(3, 5) = " << plus<int>()(3, 5) << std::endl << std::endl;
+//	std::cout << "minus<int>()(3, 5) = " << minus<int>()(3, 5) << std::endl << std::endl;
+//	std::cout << "multiplies<int>()(3, 5) = " << multiplies<int>()(3, 5) << std::endl << std::endl;
+//	std::cout << "divides<int>()(3, 5) = " << divides<int>()(3, 5) << std::endl << std::endl;
+//	std::cout << "modulus<int>()(3, 5) = " << modulus<int>()(3, 5) << std::endl << std::endl;
+//	std::cout << "negate<int>()(3) = " << negate<int>()(3) << std::endl << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}
