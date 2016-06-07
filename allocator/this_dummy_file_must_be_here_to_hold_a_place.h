47bceb765af31c08cf4075ceced19679f60dd4ad
CHEN Gonghao <chengonghao@yeah.net>
2016-06-07 10:38:52 +0800

    this dummy file must be here to hold a place

---
+++ allocator/this_dummy_file_must_be_here_to_hold_a_place.h
@@ -0,0 +1,14 @@
+/*******************************************************************
+*  Copyright(c) 2016 Chen Gonghao
+*  All rights reserved.
+*
+*  chengonghao@yeah.net
+*
+******************************************************************/
+
+#ifndef _ALLOC_PLACE_HOLDER_H_
+#define _ALLOC_PLACE_HOLDER_H_
+
+/* this_dummy_file_must_be_here_to_hold_a_place */
+
+#endif
